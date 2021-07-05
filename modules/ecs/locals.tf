761c4b4b054297de7b66f1c180600a4c922cb5d6
Lionel Martin <lionelmartin2812@gmail.com>
2021-07-05 12:34:49 +1000

    same env vars

--- modules/ecs/locals.tf
+++ modules/ecs/locals.tf
@@ -16,21 +16,21 @@ locals {
       DB_USERNAME   = var.aurora.master_username
       DB_PASSWORD   = var.aurora.master_password
 
-      AWS_BUCKET = var.s3.id
-      AWS_URL    = var.s3.bucket_domain_name
+      BUCKET_NAME = var.s3.id
+    #   AWS_URL    = var.s3.bucket_domain_name
 
-      QUEUE_CONNECTION = "sqs"
-      SQS_PREFIX       = "https://sqs.${var.region}.amazonaws.com/${var.account_id}"
-      SQS_QUEUE        = var.sqs.name
+    #   QUEUE_CONNECTION = "sqs"
+    #   SQS_PREFIX       = "https://sqs.${data.aws_region.current.name}.amazonaws.com/${var.account_id}"
+    #   SQS_QUEUE        = var.sqs.name
 
-      AWS_DEFAULT_REGION = data.aws_region.current.name
+    #   AWS_DEFAULT_REGION = data.aws_region.current.name
 
-      CACHE_DRIVER   = "redis"
-      SESSION_DRIVER = "redis"
-      REDIS_CLIENT   = "phpredis"
+    #   CACHE_DRIVER   = "redis"
+    #   SESSION_DRIVER = "redis"
+    #   REDIS_CLIENT   = "phpredis"
 
-      REDIS_HOST = var.elasticache.cache_nodes[0].address
-      REDIS_PORT = var.elasticache.cache_nodes[0].port
+    #   REDIS_HOST = var.elasticache.cache_nodes[0].address
+    #   REDIS_PORT = var.elasticache.cache_nodes[0].port
     }
   }
 }
