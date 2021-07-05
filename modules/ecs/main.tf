b3a3dd8099ccd0e39d756397477760def5a0dffb
Lionel Martin <lionelmartin2812@gmail.com>
2021-07-05 13:04:05 +1000

    merged master

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -269,22 +269,14 @@ resource "aws_ecs_service" "cron" {
 resource "aws_appautoscaling_target" "target" {
   max_capacity = var.autoscaling_max
   min_capacity = var.desired_count
-<<<<<<< HEAD
-  resource_id  = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.main.name}"
-  # role_arn = aws_iam_service_linked_role.autoscale.arn
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
-=======
   resource_id = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.main.name}"
   # role_arn = aws_iam_service_linked_role.autoscale.arn
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace = "ecs"
->>>>>>> master
 }
 
 
 resource "aws_appautoscaling_policy" "policy" {
-<<<<<<< HEAD
   name               = "autoscale"
   policy_type        = "TargetTrackingScaling"
   resource_id        = aws_appautoscaling_target.target.resource_id
@@ -294,17 +286,6 @@ resource "aws_appautoscaling_policy" "policy" {
   target_tracking_scaling_policy_configuration {
     target_value       = var.autoscaling_target
     scale_in_cooldown  = 30
-=======
-  name = "autoscale"
-  policy_type = "TargetTrackingScaling"
-  resource_id = aws_appautoscaling_target.target.resource_id
-  scalable_dimension = aws_appautoscaling_target.target.scalable_dimension
-  service_namespace = aws_appautoscaling_target.target.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    target_value = var.autoscaling_target
-    scale_in_cooldown = 30
->>>>>>> master
     scale_out_cooldown = 120
 
     predefined_metric_specification {
@@ -317,7 +298,3 @@ resource "aws_appautoscaling_policy" "policy" {
 # resource "aws_iam_service_linked_role" "autoscale" {
 #   aws_service_name = "ecs.application-autoscaling.amazonaws.com"
 # }
-<<<<<<< HEAD
-
-=======
->>>>>>> master
