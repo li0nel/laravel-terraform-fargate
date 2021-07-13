be9ebc4adba276a090ccb9fe0c1eeb388478e619
Lionel Martin <lionelmartin2812@gmail.com>
2021-07-14 07:44:33 +1000

    prevent SSM parameter overwrite

--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -2,4 +2,11 @@ resource "aws_ssm_parameter" "secret" {
   name  = "${var.stack_name}-example-secret"
   type  = "SecureString"
   value = "dummy"
+  overwrite = false
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
 }
